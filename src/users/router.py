2b44a6b6faa86b68bc8e44cd6bfbe18d6c4dc779
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-03-24 19:27:00 +0700

    Add a route to get the user by uuid

--- src/users/router.py
+++ src/users/router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+import uuid
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from ..database import get_db
@@ -15,3 +17,11 @@ router = APIRouter(
 def get_users(pagination: Pagination = Depends(), db: Session = Depends(get_db)):
     users = service.get_users(db, pagination.skip, pagination.limit)
     return users
+
+
+@router.get('/{user_id}', response_model=schemas.UserResponse)
+def get_user(user_id: uuid.UUID, db: Session = Depends(get_db)):
+    user = service.get_user(db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    return user
--- src/users/service.py
+++ src/users/service.py
@@ -5,3 +5,7 @@ from .models import User
 
 def get_users(db: Session, skip: int, limit: int) -> list[User]:
     return db.query(User).offset(skip).limit(limit).all()
+
+
+def get_user(db: Session, user_id: int) -> User | None:
+    return db.query(User).filter(User.id == user_id).first()
