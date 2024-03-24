e90fb17d05872e815913d1d297678b190ea63886
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-03-24 18:03:59 +0700

    Add pagination for the route to get all users

--- src/users/router.py
+++ src/users/router.py
@@ -3,14 +3,15 @@ from sqlalchemy.orm import Session
 
 from ..database import get_db
 from . import schemas, service
+from .pagination import Pagination
 
 router = APIRouter(
     prefix='/api/v1/users',
-    tags=['users'],
+    tags=['users']
 )
 
 
 @router.get('/', response_model=list[schemas.UserResponse])
-def get_users(db: Session = Depends(get_db)):
-    users = service.get_users(db)
+def get_users(pagination: Pagination = Depends(), db: Session = Depends(get_db)):
+    users = service.get_users(db, pagination.skip, pagination.limit)
     return users
