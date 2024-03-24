e5c551fc55ce1db3f36a03127bd817aa3637d829
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-03-24 20:07:09 +0700

    Add password validation

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,7 +1,8 @@
 import datetime
+import re
 import uuid
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ValidationInfo, field_validator
 
 
 class UserBase(BaseModel):
@@ -16,6 +17,32 @@ class UserCreate(UserBase):
     email: EmailStr
     password: str
     password_confirm: str
+    
+    @field_validator('password')
+    def validate_password(cls, password: str, info: ValidationInfo) -> str:
+        uppercase_regex = re.compile(r'[A-Z]')
+        lowercase_regex = re.compile(r'[a-z]')
+        digit_regex = re.compile(r'[0-9]')
+        special_char_regex = re.compile(r'[!@#$%^&*()_+{}|:<>?~\-=\[\];\',./]')
+
+        if len(password) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        if not re.search(uppercase_regex, password):
+            raise ValueError('Password must have at least one capital letter')
+        if not re.search(lowercase_regex, password):
+            raise ValueError('Password must have at least one lowercase letter')
+        if not re.search(digit_regex, password):
+            raise ValueError('Password must have at least one digit')
+        if not re.search(special_char_regex, password):
+            raise ValueError('Password must have at least one special character')
+        
+        return password
+
+    @field_validator('password_confirm')
+    def validate_password_confirm(cls, password_confirm: str, info: ValidationInfo) -> str:
+        if password_confirm != info.data.get('password'):
+            raise ValueError("Passwords don't match")
+        return password_confirm
 
 
 class UserResponse(UserBase):
